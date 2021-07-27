41f4c2b65db83717df4e6fe4b374e10125be8b76
panda <iospandaman@gmail.com>
2021-07-28 03:25:19 +0900

    Add XcodeProject

---
+++ TalkToMBTI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TalkToMBTI
+//
+//  Created by Panda on 2021/07/28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
